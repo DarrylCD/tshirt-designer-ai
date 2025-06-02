86ece88602d9bc702c1d239f514bab8ca4819db5
DarrylCD <daredarryl@rediffmail.com>
2025-06-02 16:28:23 +0530

    feature: add components

---
+++ client/src/components/index.js
@@ -0,0 +1,16 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+// import TextPicekr from "./TextPicker";
+import Tab from "./Tab";
+import { getContrastingColor } from "../config/helpers";
+
+export { 
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker, 
+    // TextPicker,
+     Tab,         
+    };
