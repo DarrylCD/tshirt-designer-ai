dfc6bcc2241b4fba409a599c2bdb7621a15a9a23
DarrylCD <daredarryl@rediffmail.com>
2025-09-02 16:51:44 +0530

    refactor: add updated server deployment url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: import.meta.env.VITE_BACKEND_URL + "api/v1/dalle",
   },
 };
 
